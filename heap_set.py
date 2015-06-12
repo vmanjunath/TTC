b387c219e0bff05fb78a8ebf8570c04da60f3ae5
Vikram Manjunath <vikramma@gmail.com>
2015-06-12 07:39:30 -0400

    Cleaned up heap_set.py for PyLint

--- heap_set.py
+++ heap_set.py
@@ -1,7 +1,18 @@
+"""
+HeapSet class defines an object that consists of a Fibonacci heap as well as a set.
+fibonacc_heap_mod permits duplicate entries while we need elements to be unique.
+
+Keeping a set alongside allows O(1) lookup to verify membership and prevent duplicate
+insertion into th heap. The only other operation on the set is deletion which is also O(1).
+"""
 from fibonacci_heap_mod import Fibonacci_heap
 
 
 class HeapSet(object):
+    """
+    fibonacci_heap + set = HeapSet
+    includes a priority order for heap insertion and lookup
+    """
     def __init__(self, priority):
         self.heap = Fibonacci_heap()
         self.set = set({})
@@ -11,11 +22,17 @@ class HeapSet(object):
         return '<HeapSet({})>'.format(self.set)
 
     def add(self, element):
+        """
+        Add element only if it isn't already in.
+        """
         if element not in self.set:
             self.set.add(element)
             self.heap.enqueue(element, self.priority(element))
 
     def pop(self):
+        """
+        Remove the min element
+        """
         elem = self.heap.dequeue_min().get_value()
         self.set.remove(elem)
         return elem
